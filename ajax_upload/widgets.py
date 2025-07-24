01432516ead2a5632f674dff9e63c37ed0ffe111
Vladimir Caruntu <vladimir.caruntu@gmail.com>
2025-07-24 15:07:10 +0300

    Support for Django 5.2

--- ajax_upload/widgets.py
+++ ajax_upload/widgets.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.core.files import File
 from django.urls import reverse
 from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 
 import urllib.request, urllib.error, urllib.parse
 
